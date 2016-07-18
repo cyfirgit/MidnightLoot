f79be1adca9ae41350ec96881a7d2fa81b8bf4b2
cyfirgit <cyfir@cox.net>
2016-07-17 23:37:56 -0700

    17 July
    -Restructured classReference to put specs into their own subtable.
    -Edits to AddLoot (changed loot var to item)
    -Started CHAT_MSG_ADDON handler
    -Added error logging
    -Implemented eligibility checks
    -Implemented loot drop detection
    -Refactored spec-filter eligibility code for trinkets and relics

--- MidnightLoot.lua
+++ MidnightLoot.lua
@@ -9,88 +9,113 @@ MidnightLoot.classReference = {
 	['MAGE'] = {
 		['index'] = 8,
 		['armor'] = 'Cloth',
-		[62] = "Arcane",
-		[63] = "Fire",
-		[64] = "Frost",
+		['specs'] = {
+			[62] = "Arcane",
+			[63] = "Fire",
+			[64] = "Frost",
+		},
 	},
 	['PALADIN'] = {
 		['index'] = 2,
 		['armor'] = 'Plate',
-		[65] = "Holy",
-		[66] = "Protection",
-		[70] = "Retribution",
+		['specs'] = {
+			[65] = "Holy",
+			[66] = "Protection",
+			[70] = "Retribution",
+		},
 	},
 	['WARRIOR'] = {
 		['index'] = 1,
-		['armor'] = 'Plate',		
-		[71] = "Arms",
-		[72] = "Fury",
-		[73] = "Protection",
+		['armor'] = 'Plate',
+		['specs'] = {		
+			[71] = "Arms",
+			[72] = "Fury",
+			[73] = "Protection",
+		},
 	},
 	['DRUID'] = {
 		['index'] = 11,
-		['armor'] = 'Leather',		
-		[102] = "Balance",
-		[103] = "Feral",
-		[104] = "Guardian",
-		[105] = "Restoration",
+		['armor'] = 'Leather',
+		['specs'] = {	
+			[102] = "Balance",
+			[103] = "Feral",
+			[104] = "Guardian",
+			[105] = "Restoration",
+		},
 	},
 	['DEATHKNIGHT'] = {
 		['index'] = 6,
 		['armor'] = 'Plate',
-		[250] = "Blood",
-		[251] = "Frost",
-		[252] = "Unholy",
+		['specs'] = {
+			[250] = "Blood",
+			[251] = "Frost",
+			[252] = "Unholy",
+		},
 	},
 	['HUNTER'] = {
 		['index'] = 3,
 		['armor'] = 'Mail',
-		[253] = "Beast Mastery",
-		[254] = "Marksmanship",
-		[255] = "Survival",
+		['specs'] = {
+			[253] = "Beast Mastery",
+			[254] = "Marksmanship",
+			[255] = "Survival",
+		},
 	},
 	['PRIEST'] = {
 		['index'] = 5,
 		['armor'] = 'Cloth',
-		[256] = "Discipline",
-		[257] = "Holy",
-		[258] = "Shadow",
+		['specs'] = {
+			[256] = "Discipline",
+			[257] = "Holy",
+			[258] = "Shadow",
+		},
 	},
 	['ROGUE'] = {
 		['index'] = 4,
-		['armor'] = 'Leather',		
-		[259] = "Assassination",
-		[260] = "Outlaw",
-		[261] = "Subtlety",
+		['armor'] = 'Leather',
+		['specs'] = {	
+			[259] = "Assassination",
+			[260] = "Outlaw",
+			[261] = "Subtlety",
+		},
 	},
 	['SHAMAN'] = {
 		['index'] = 7,
-		['armor'] = 'Mail',		
-		[262] = "Elemental",
-		[263] = "Enhancement",
-		[264] = "Restoration",
+		['armor'] = 'Mail',
+		['specs'] = {
+			[262] = "Elemental",
+			[263] = "Enhancement",
+			[264] = "Restoration",
+		},
 	},
 	['WARLOCK'] = {
 		['index'] = 9,
-		['armor'] = 'Cloth',		
-		[265] = "Affliction",
-		[266] = "Demonology",
-		[267] = "Destruction",
+		['armor'] = 'Cloth',
+		['specs'] = {
+			[265] = "Affliction",
+			[266] = "Demonology",
+			[267] = "Destruction",
+		},
 	},
 	['MONK'] = {
 		['index'] = 10,
-		['armor'] = 'Leather',		
-		[268] = "Brewmaster",
-		[269] = "Windwalker",
-		[270] = "Mistweaver",
+		['armor'] = 'Leather',
+		['specs'] = {		
+			[268] = "Brewmaster",
+			[269] = "Windwalker",
+			[270] = "Mistweaver",
+		},
 	},
 	['DEMONHUNTER'] = {
 		['index'] = 12,
-		['armor'] = 'Leather',		
-		[577] = "Havoc",
-		[581] = "Vengeance",
+		['armor'] = 'Leather',
+		['specs'] = {		
+			[577] = "Havoc",
+			[581] = "Vengeance",
+		},
 	},
 }
+MidnightLoot.errors = {}
 
 function MidnightLoot_OnLoad(self)
 	self:Show();
@@ -128,20 +153,21 @@ end
 -- end
 -- Consolidated function for updating the two loot frame buttons according to state.
 
-function MidnightLoot.AddLoot(itemID, owner)
-	local loot = {};
-	loot.ID = itemID;
-	loot.owner = owner;
-	loot.name, loot.link, loot.quality, loot.iLevel, loot.reqLevel, loot.class, loot.subclass, loot.maxStack, loot.equipSlot, loot.texture, loot.vendorPrice = GetItemInfo(itemID)
+function MidnightLoot.AddLoot(itemLink, itemID, owner)
+	local item = {};
+	item.owner = owner;
+	item.ID = itemID;
+	item.link = itemLink
+	item.name, _, item.quality, item.iLevel, item.reqLevel, item.class, item.subclass, item.maxStack, item.equipSlot, item.texture, item.vendorPrice = GetItemInfo(itemID)
 	--If it's an artifact relic, tag it with the appropriate type
-	if loot.subclass == "Artifact Relic" then
-		_,_, loot.relicType = C_ArtifactUI.GetRelicInfoByItemID(itemID)
+	if item.subclass == "Artifact Relic" then
+		_,_, item.relicType = C_ArtifactUI.GetRelicInfoByItemID(itemID)
 	end
-	if loot.name ~= nil then
-		table.insert(MidnightLoot.activeLootItems, loot)
+	if item.name ~= nil then
+		table.insert(MidnightLoot.activeLootItems, item)
 		MidnightLoot.UpdateActiveLoot()
 	else
-		print("ID "..itemID.." is not a real item, or has not been added to your loot database yet.  Sorry!")
+		SendAddonMessage("MidnightLoot", "ERROR~".."ID "..itemID.." has nil response.", "RAID")
 	end
 	return getn(MidnightLoot.activeLootItems);
 end
@@ -152,6 +178,7 @@ function MidnightLoot.RemoveLoot(index)
 end
 
 function MidnightLoot.UpdateLootButtons(self, state1, text1, state2, text2)
+-- Change what the loot button frame displays.  See MidnightLoot.UpdateActiveLoot for context.
 local buttons = {{self.button1, state1, text1}, {self.button2, state2, text2}}
 	for i = 1, 2 do
 		local button = buttons[i][1]
@@ -196,7 +223,7 @@ function MidnightLoot.UpdateActiveLoot()
 			button.itemName:SetText(button.item.name)
 			button.itemName:SetVertexColor(button.color.r, button.color.g, button.color.b)
 			-- if the item belongs to the player, give them the option to claim or release it.
-			if button.item.status == "Claimable" then
+			if button.item.status == "Pending" and button.item.owner == MidnightLoot.playerName then
 				MidnightLoot.UpdateLootButtons(button, "enabled", "Claim", "enabled", "Release")				
 			-- if the player is eligible to roll on the item, give them the roll buttons.
 			elseif button.item.status == "Eligible" then
@@ -206,10 +233,10 @@ function MidnightLoot.UpdateActiveLoot()
 				MidnightLoot.UpdateLootButtons(button, "disabled", "Main Spec", "enabled", "Offspec")				
 			-- if the player is supposed to trade the item, give them the trade button.
 			elseif button.item.status == "Tradeable" then
-				MidnightLoot.UpdateLootButtons(button, "hidden", " ", "enabled", "Trade")			
+				MidnightLoot.UpdateLootButtons(button, "hidden", "", "enabled", "Trade")			
 			-- in all other states, the buttons are hidden.
 			else
-				MidnightLoot.UpdateLootButtons(button, "hidden", " ", "hidden", "")
+				MidnightLoot.UpdateLootButtons(button, "hidden", "", "hidden", "")
 			end
 			
 			button:Show();
@@ -225,12 +252,13 @@ end
 function MidnightLoot.UpdateLootItem(itemID, owner, ownerStatus)
 	--Process new loot information
 	local numItems = getn(MidnightLoot.activeLootItems);
-	local isNewItem = true;
+	--local isNewItem = true;
 	local index = 0;
-	local eligibleSpecs = MidnightLoot.RequestEligibleSpecs();
+	local eligibleSpecs = MidnightLoot.eligibleSpecs[MidnightLoot.playerName];
 	local isEligible = false;
 	local playerClass = MidnightLoot.classReference[MidnightLoot.playerClass]['index']
 	
+	--[[Leaving this in here in case I revert, but UpdateLootItem should only ever fire after an item is already in ActiveLootItems now.
 	--Check whether the item already exists in the activeLootItems.
 	for i = 1, numItems do
 		if MidnightLoot.activeLootItems[i].ID == itemID and MidnightLoot.activeLootItems[i].owner == owner then
@@ -242,75 +270,51 @@ function MidnightLoot.UpdateLootItem(itemID, owner, ownerStatus)
 	if isNewItem == true then
 		index = MidnightLoot.AddLoot(itemID, owner)
 	end
+	]]--
 	
 	--Starting with ownerStatus, determine what the status of this loot item is to the player.
 	local item = MidnightLoot.activeLootItems[index]
 	-- PROBABLY SHOULD BUILD THE CHECKS FOR THE PLAYER'S OWN LOOT INTO HERE
 	if ownerStatus == "Claimed" then
-		MidnightLoot.activeLootItems[index].status = "Claimed"
+		item.status = "Claimed"
 	elseif ownerStatus == "Locked" then
-		MidnightLoot.activeLootItems[index].status = "Locked"
+		item.status = "Locked"
 	elseif ownerStatus == "Pending" then
-		MidnightLoot.activeLootItems[index].status = "Pending"		
+		item.status = "Pending"		
 	elseif ownerStatus == "Released" then
-		if item.equipSlot == "INVTYPE_TRINKET" or item.subclass == "Artifact Relic" then
-			EJ_SelectInstance(MidnightLoot.instanceID)
-			--Cycle through main spec(s), filtering the instance's loot in the Encounter Journal.  If it is found on a main spec's loot list, flag it eligible.
-			for specIndex, spec in ipairs(eligibleSpecs['main']) do
-				EJ_SetLootFilter(playerClass, spec)
-				for i = 1, EJ_GetNumLoot() do
-					local _,_,_,_,ejItemID = GetLootInfoByIndex(i)
-					if ejItemID == item.ID then
-						MidnightLoot.activeLootItems[index].status = "Eligible"
-						break
-					end
-				end
-				if MidnightLoot.activeLootItems[index].status == "Eligible" then
-					break
-				end
-			end				
-			--If it's not on the main spec list, repeat the process using the offspec list.  If found flag Eligible - Offspec.
-			if isEligible == false then
-				for specIndex, spec in ipairs(eligibleSpecs['off']) do
-					EJ_SetLootFilter(playerClass, spec)
+		if owner ~= MidnightLoot.playerName then
+			if item.equipSlot == "INVTYPE_TRINKET" or item.subclass == "Artifact Relic" then
+				--Cycle through the instance's loot, filtered by spec.
+				--When/if the item is found in the spec-filtered list, upgrade the item's status as applicable from ineligible -> offspec -> main spec.
+				--If it's never found, then it's not eligible.
+				EJ_SelectInstance(MidnightLoot.instanceID)
+				local statusRanking = {"Ineligible" = 0, "Eligible - Offspec" = 1, "Eligible" = 2}; 
+				item.status = "Ineligible";
+				for specID, eligibility in ipairs(eligibleSpecs) do
+					EJ_SetLootFilter(playerClass, specID);
 					for i = 1, EJ_GetNumLoot() do
-						local _,_,_,_,ejItemID = GetLootInfoByIndex(i)
-						if ejItemID == item.ID then
-							MidnightLoot.activeLootItems[index].status = "Eligible"
-							break
+						local _,_,_,_,ejItemID = GetLootInfoByIndex(i);
+						if ejItemID == item.ID and statusRanking[eligibility] > statusRanking[item.status] then
+							item.status = eligibility;
 						end
 					end
-					if MidnightLoot.activeLootItems[index].status == "Eligible" then
-						break
-					end
 				end
-			--Otherwise set Ineligible.
-			else
-				MidnightLoot.activeLootItems[index].status = "Ineligible"
+			elseif item.class == "Armor" then -- *** It may be easier to just use the above code for all items.
+				if item.subclass == "Miscellaneous" or item.subclass == MidnightLoot.classReference[MidnightLoot.playerClass]['armor'] then
+					item.status = "Eligible"
+				else
+					item.status = "Ineligible"
+				end
 			end
-		elseif item.class == "Armor" then
-			if item.subclass == "Miscellaneous" or item.subclass == MidnightLoot.classReference[MidnightLoot.playerClass]['armor'] then
-				MidnightLoot.activeLootItems[index].status = "Eligible"
-			else
-				MidnightLoot.activeLootItems[index].status = "Ineligible"
-			end
-		end
+		else
+			item.status == "Released"
+		end 
 	else
-		print("Could not determine status!")
+		SendAddonMessage("MidnightLoot", "ERROR~".."Status of '"..ownerStatus.."' failed to parse.", "RAID");
 	end
 end
 
-function MidnightLoot.RequestEligibleSpecs()
-	if IsRaidLeader() or UnitIsGroupAssistant('PLAYER') then
-		--Check local tables
-	else
-		--Request table update
-	end
-	
-	return nil
-end
-
-function MidnightLoot.SetInstanceID() -- NEED A HOOK TO FIRE THIS WHEN ENTERING AN INSTANCE OR LOADING THE ADDON.
+function MidnightLoot.SetInstanceID() -- ***NEED A HOOK TO FIRE THIS WHEN ENTERING AN INSTANCE OR LOADING THE ADDON.
 	MidnightLoot.instanceID = EJ_GetCurrentInstance()
 end
 
@@ -347,10 +351,38 @@ function MidnightLoot_EventHandler(self, event, ...)
 			else
 				status = "Locked"
 			end
-			SendAddonMessage("MidnightLoot", "NEW_LOOT~"..itemLink.."~"..playerName.."~"..status, "RAID")
+			SendAddonMessage("MidnightLoot", "NEW_LOOT~"..itemLink.."~"..itemID.."~"..status, "RAID")
 		end
 	elseif event == "CHAT_MSG_ADDON" and ... == "MidnightLoot" then
 		local _, messageStr, _, sender = ...;
 		local messageData = {strsplit("~", messageStr)};
-		if messageData[1] == --STOPHERE
+		if messageData[1] == "NEW_LOOT" then
+			local _, itemLink, itemID, status = messageData;
+			MidnightLoot.AddLoot(itemLink, itemID, sender);
+		elseif messageData[1] == "UPDATE_LOOT" then
+			local _, itemID, owner, status = messageData;
+			MidnightLoot.UpdateLootItem(itemID, owner, status);
+		elseif messageData[1] == "UPDATE_SPECS" then --*** function to update specs should determine what players' changed and only update those.
+			--[[Example of output:
+					MidnightLoot.eligibleSpecs['Volstatsz-AeriePeak'] = {
+						[250] = 'Eligible',
+						[251] = 'Eligible - Offspec',
+						[252] = 'Ineligible',
+						}
+			]]--
+			local _, player, playerClass, spec1, spec2, spec3, spec4 = messageData;
+			local specs = {};
+			local eligibility = {spec1, spec2, spec3, spec4};
+			for specID, _ in ipairs(MidnightLoot.classReference[playerClass].specs) do
+				table.insert(specs, specID);
+			end
+			for i = 1, 4 do
+				if (i <= 2 or playerClass ~= "DEMONHUNTER") and (i <= 3 or playerClass == "DRUID") then
+					MidnightLoot.eligibleSpecs[player][specs[i]] = eligibility[i];
+				end
+			end --STOPHERE (Adding chat message handling for known events.  I need to take the stuff out of "UPDATE_SPECS" and put it in its own function for consistency.)
+		elseif messageData[1] == "ERROR" then
+			table.insert(MidnightLoot.errors, {player = sender, timestamp = GetTime(), message = sendmessageData[2]});
+		end	
+	end
 end
