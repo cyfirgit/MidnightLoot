659f37288d8446e24de96e3ebbc9be052e9c5196
cyfirgit <cyfir@cox.net>
2016-07-23 18:32:37 -0700

    23 July
    - Lots of roll handling
    - Reorganized functions into concept groups and added some labeling

--- MidnightLoot.lua
+++ MidnightLoot.lua
@@ -1,10 +1,14 @@
 local addonEnabled = true;
-MidnightLoot = {};
 MidnightLootSaved = {};
+MidnightLoot = {};
 MidnightLoot.LOOT_ITEM_HEIGHT = 50;
+MidnightLoot.ROLL_KINDS = {'main', 'off', 'pass'}
 MidnightLoot.activeLootItems = {};
 _, MidnightLoot.playerClass = UnitClass('PLAYER');
 MidnightLoot.playerName = {UnitName('PLAYER')};
+MidnightLoot.errors = {}
+MidnightLoot.PREFIX = 'MidnightLoot'
+MidnightLoot.activeRoster = {}
 MidnightLoot.classReference = {
 	['MAGE'] = {
 		['index'] = 8,
@@ -115,7 +119,10 @@ MidnightLoot.classReference = {
 		},
 	},
 }
-MidnightLoot.errors = {}
+
+
+--[[* * * * * UI & Utility * * * * *]]--
+
 
 function MidnightLoot_OnLoad(self)
 	self:Show();
@@ -134,75 +141,94 @@ function MidnightLoot_OnLoad(self)
 	MidnightLoot.UpdateActiveLoot();
 end
 
-MidnightLoot_OnDragStart = function(self)
-	self:StartMoving();
-end
+--***function MidnightLoot_Show(self)
 
-MidnightLoot_OnDragStop = function(self)
-	self:StopMovingOrSizing();
-end
-
-MidnightLoot_Hide = function(self)
+function MidnightLoot_Hide(self)
 	self:Hide();
 end
 
--- function MidnightLoot.CalculateDynamic(offset)
-	-- local past = math.floor(offset / MidnightLoot.LOOT_ITEM_HEIGHT)
-	-- local partial = offset % MidnightLoot.LOOT_ITEM_HEIGHT
-	-- return past, partial
--- end
--- Consolidated function for updating the two loot frame buttons according to state.
-
-function MidnightLoot.AddLoot(itemLink, itemID, owner)
-	local item = {};
-	item.owner = owner;
-	item.ID = itemID;
-	item.link = itemLink
-	item.name, _, item.quality, item.iLevel, item.reqLevel, item.class, item.subclass, item.maxStack, item.equipSlot, item.texture, item.vendorPrice = GetItemInfo(itemID)
-	--If it's an artifact relic, tag it with the appropriate type
-	if item.subclass == "Artifact Relic" then
-		_,_, item.relicType = C_ArtifactUI.GetRelicInfoByItemID(itemID)
-	end
-	if item.name ~= nil then
-		table.insert(MidnightLoot.activeLootItems, item)
-		MidnightLoot.UpdateActiveLoot()
-	else
-		SendAddonMessage("MidnightLoot", "ERROR~".."ID "..itemID.." has nil response.", "RAID")
-	end
-	return getn(MidnightLoot.activeLootItems);
+function MidnightLoot_OnDragStart(self)
+	self:StartMoving();
 end
 
-function MidnightLoot.RemoveLoot(index)
-	table.remove(MidnightLoot.activeLootItems, index)
-	MidnightLoot.UpdateActiveLoot()
+function MidnightLoot_OnDragStop(self)
+	self:StopMovingOrSizing();
 end
-
-function MidnightLoot.UpdateLootButtons(self, state1, text1, state2, text2)
--- Change what the loot button frame displays.  See MidnightLoot.UpdateActiveLoot for context.
-local buttons = {{self.button1, state1, text1}, {self.button2, state2, text2}}
-	for i = 1, 2 do
-		local button = buttons[i][1]
-		local state = buttons[i][2]
-		local text = buttons[i][3]
-		if state == "hidden" then
-			button:Hide();
-		elseif state == "enabled" then
-			button:SetText(text);
-			button:Enable();
-			button:Show();
-		elseif state == "disabled" then
-			button:SetText(text);
-			button:Disable();
-			button:Show();
-		end;
-	end;
-end;
 
 function MidnightLoot_LootIcon_OnEnter(self)
 	local parent = self:GetParent();
 	GameTooltip:SetOwner(self, "ANCHOR_RIGHT");
 	GameTooltip:SetItemByID(parent.item.ID);
 	CursorUpdate(self);
+end
+
+function MidnightLoot.BossKilled(...)
+	MidnightLoot.lastBoss = ...
+	--Update the raid roster.
+	table.wipe(MidnightLoot.activeRoster)
+	for i = 1, GetNumGroupMembers() do
+		local name, realm = UnitName("Raid"..i)
+		local _, playerClass = UnitClass("Raid"..i)
+		local player = name.."-"..realm
+		MidnightLoot.activeRoster[player] = {
+			['playerClass'] = playerClass,
+			['isInRollOff'] = false,
+			['rollOffRolls'] = {};
+		}
+	end
+end
+
+function MidnightLoot.UpdateSpecs(messageData)
+	--*** function to update specs should determine what players' changed and only update those.
+	--[[Example of output:
+			MidnightLoot.eligibleSpecs['Volstatsz-AeriePeak'] = {
+				[250] = 'Eligible',
+				[251] = 'Eligible - Offspec',
+				[252] = 'Ineligible',
+				}
+	]]--
+	local _, player, playerClass, spec1, spec2, spec3, spec4 = messageData;
+	local specs = {};
+	local eligibility = {spec1, spec2, spec3, spec4};
+	for specID, _ in ipairs(MidnightLoot.classReference[playerClass].specs) do
+		table.insert(specs, specID);
+	end
+	for i = 1, 4 do
+		if (i <= 2 or playerClass ~= "DEMONHUNTER") and (i <= 3 or playerClass == "DRUID") then
+			MidnightLoot.eligibleSpecs[player][specs[i]] = eligibility[i];
+		end
+	end
+end
+
+function MidnightLoot_EventHandler(self, event, ...)
+	if event == "BOSS_KILL" then
+		MidnightLoot.BossKilled(...)
+	elseif event == "ENCOUNTER_LOOT_RECEIVED" then
+		MidnightLoot.LootDropped(...)
+	elseif event == "CHAT_MSG_ADDON" and ... == "MidnightLoot" then
+		local _, messageStr, _, sender = ...;
+		local messageData = {strsplit("~", messageStr)};
+		if messageData[1] == "NEW_LOOT" then
+			local _, itemLink, itemID, status = messageData;
+			MidnightLoot.AddLoot(itemLink, itemID, sender);
+		elseif messageData[1] == "UPDATE_LOOT" then
+			local _, itemID, owner, status = messageData;
+			MidnightLoot.UpdateLootItem(itemID, owner, status);
+		elseif messageData[1] == "UPDATE_SPECS" then 
+			MidnightLoot.UpdateSpecs(messageData)
+		elseif messageData[1] == "INTENT" then
+			local _, itemID, owner, kind = messageData
+			MidnightLoot.NewIntent(sender, itemID, owner, kind)
+		elseif messageData[1] == "WINNER" then
+			local _, itemID, owner, player, kind = messageData
+			MidnightLoot.ItemWon(itemID, owner, player, kind)
+		elseif messageData[1] == "ROLLOFF_START" then
+			RandomRoll(1,100)
+		elseif messageData[1] == "ERROR" then
+			local errorMessage = messageData[2]
+			table.insert(MidnightLoot.errors, {player = sender, timestamp = GetTime(), message = errorMessage});
+		end	
+	end
 end
 
 function MidnightLoot.UpdateActiveLoot()
@@ -249,32 +275,99 @@ function MidnightLoot.UpdateActiveLoot()
 	HybridScrollFrame_Update(scrollFrame, totalHeight, scrollFrame:GetHeight())
 end
 
+function MidnightLoot.UpdateLootButtons(self, state1, text1, state2, text2)
+-- Change what the loot button frame displays.  See MidnightLoot.UpdateActiveLoot for context.
+local buttons = {{self.button1, state1, text1}, {self.button2, state2, text2}}
+	for i = 1, 2 do
+		local button = buttons[i][1]
+		local state = buttons[i][2]
+		local text = buttons[i][3]
+		if state == "hidden" then
+			button:Hide();
+		elseif state == "enabled" then
+			button:SetText(text);
+			button:Enable();
+			button:Show();
+		elseif state == "disabled" then
+			button:SetText(text);
+			button:Disable();
+			button:Show();
+		end;
+	end;
+end;
+
+function MidnightLoot.RemoveLoot(index)
+	table.remove(MidnightLoot.activeLootItems, index)
+	MidnightLoot.UpdateActiveLoot()
+end
+
+function MidnightLoot.SetInstanceID() -- ***NEED A HOOK TO FIRE THIS WHEN ENTERING AN INSTANCE OR LOADING THE ADDON.
+	MidnightLoot.instanceID = EJ_GetCurrentInstance()
+end
+
+--[[function MidnightLoot.CalculateDynamic(offset)
+	local past = math.floor(offset / MidnightLoot.LOOT_ITEM_HEIGHT)
+	local partial = offset % MidnightLoot.LOOT_ITEM_HEIGHT
+	return past, partial
+end]]
+
+function MidnightLoot.GetActiveLootIndex(itemID, owner)
+	--Returns the index of a unique item in the activeLootItems table.
+	for index, item in ipairs(MidnightLoot.activeLootItems) do
+		if item.ID == itemID and item.owner == owner then
+			return index
+		end
+	end
+end
+
+
+--[[* * * * * Item Detection & Categorization * * * * *]]--
+
+
+function MidnightLoot.LootDropped(...)
+	local encounterID, itemID, itemLink, quantity, playerName, className = ...;
+	local status = "";
+	if encounterID == MidnightLoot.lastBoss and playerName == MidnightLoot.playerName then
+		if MidnightLoot.TestTradable(itemLink) then
+			status = "Pending"
+		else
+			status = "Locked"
+		end
+		SendAddonMessage(MidnightLoot.PREFIX, "NEW_LOOT~"..itemLink.."~"..itemID.."~"..status, "RAID")
+	end
+end
+
+function MidnightLoot.AddLoot(itemLink, itemID, owner)
+	local item = {};
+	item.results = {};
+	item.owner = owner;
+	item.ID = itemID;
+	item.link = itemLink
+	item.name, _, item.quality, item.iLevel, item.reqLevel, item.class, item.subclass, item.maxStack, item.equipSlot, item.texture, item.vendorPrice = GetItemInfo(itemID)
+	--If it's an artifact relic, tag it with the appropriate type
+	if item.subclass == "Artifact Relic" then
+		_,_, item.relicType = C_ArtifactUI.GetRelicInfoByItemID(itemID)
+	end
+	if item.name ~= nil then
+		table.insert(MidnightLoot.activeLootItems, item)
+		MidnightLoot.UpdateActiveLoot()
+	else
+		SendAddonMessage(MidnightLoot.PREFIX, "ERROR~".."ID "..itemID.." has nil response.", "RAID")
+	end
+	return getn(MidnightLoot.activeLootItems);
+end
+
 function MidnightLoot.UpdateLootItem(itemID, owner, ownerStatus)
-	--Process new loot information
+	--Update loot information for an existing item.
 	local numItems = getn(MidnightLoot.activeLootItems);
-	--local isNewItem = true;
-	local index = 0;
+	local index = MidnightLoot.GetActiveLootIndex(itemID, owner)
 	local eligibleSpecs = MidnightLoot.eligibleSpecs[MidnightLoot.playerName];
 	local isEligible = false;
 	local playerClass = MidnightLoot.classReference[MidnightLoot.playerClass]['index']
 	
-	--[[Leaving this in here in case I revert, but UpdateLootItem should only ever fire after an item is already in ActiveLootItems now.
-	--Check whether the item already exists in the activeLootItems.
-	for i = 1, numItems do
-		if MidnightLoot.activeLootItems[i].ID == itemID and MidnightLoot.activeLootItems[i].owner == owner then
-			index = i
-			isNewItem = false
-		end
-	end
-	--If, after iterating through the active items, we determine this is a new loot item, add it to activeLootItems.
-	if isNewItem == true then
-		index = MidnightLoot.AddLoot(itemID, owner)
-	end
-	]]--
-	
 	--Starting with ownerStatus, determine what the status of this loot item is to the player.
 	local item = MidnightLoot.activeLootItems[index]
-	-- PROBABLY SHOULD BUILD THE CHECKS FOR THE PLAYER'S OWN LOOT INTO HERE
+	-- ***PROBABLY SHOULD BUILD THE CHECKS FOR THE PLAYER'S OWN LOOT INTO HERE
 	if ownerStatus == "Claimed" then
 		item.status = "Claimed"
 	elseif ownerStatus == "Locked" then
@@ -288,7 +381,7 @@ function MidnightLoot.UpdateLootItem(itemID, owner, ownerStatus)
 				--When/if the item is found in the spec-filtered list, upgrade the item's status as applicable from ineligible -> offspec -> main spec.
 				--If it's never found, then it's not eligible.
 				EJ_SelectInstance(MidnightLoot.instanceID)
-				local statusRanking = {"Ineligible" = 0, "Eligible - Offspec" = 1, "Eligible" = 2}; 
+				local statusRanking = {["Ineligible"] = 0, ["Eligible - Offspec"] = 1, ["Eligible"] = 2}; 
 				item.status = "Ineligible";
 				for specID, eligibility in ipairs(eligibleSpecs) do
 					EJ_SetLootFilter(playerClass, specID);
@@ -307,15 +400,11 @@ function MidnightLoot.UpdateLootItem(itemID, owner, ownerStatus)
 				end
 			end
 		else
-			item.status == "Released"
+			item.status = "Released"
 		end 
 	else
-		SendAddonMessage("MidnightLoot", "ERROR~".."Status of '"..ownerStatus.."' failed to parse.", "RAID");
+		SendAddonMessage(MidnightLoot.PREFIX, "ERROR~".."Status of '"..ownerStatus.."' failed to parse.", "RAID");
 	end
-end
-
-function MidnightLoot.SetInstanceID() -- ***NEED A HOOK TO FIRE THIS WHEN ENTERING AN INSTANCE OR LOADING THE ADDON.
-	MidnightLoot.instanceID = EJ_GetCurrentInstance()
 end
 
 function MidnightLoot.TestTradable(itemLink)
@@ -339,50 +428,165 @@ function MidnightLoot.TestTradable(itemLink)
 	return isBoundTradable
 end
 
-function MidnightLoot_EventHandler(self, event, ...)
-	if event == "BOSS_KILL" then
-		MidnightLoot.lastBoss = ...;
-	elseif event == "ENCOUNTER_LOOT_RECEIVED" then
-		local encounterID, itemID, itemLink, quantity, playerName, className = ...;
-		local status = "";
-		if encounterID == MidnightLoot.lastBoss and playerName == MidnightLoot.playerName then
-			if MidnightLoot.TestTradable(itemLink) then
-				status = "Pending"
-			else
-				status = "Locked"
-			end
-			SendAddonMessage("MidnightLoot", "NEW_LOOT~"..itemLink.."~"..itemID.."~"..status, "RAID")
-		end
-	elseif event == "CHAT_MSG_ADDON" and ... == "MidnightLoot" then
-		local _, messageStr, _, sender = ...;
-		local messageData = {strsplit("~", messageStr)};
-		if messageData[1] == "NEW_LOOT" then
-			local _, itemLink, itemID, status = messageData;
-			MidnightLoot.AddLoot(itemLink, itemID, sender);
-		elseif messageData[1] == "UPDATE_LOOT" then
-			local _, itemID, owner, status = messageData;
-			MidnightLoot.UpdateLootItem(itemID, owner, status);
-		elseif messageData[1] == "UPDATE_SPECS" then --*** function to update specs should determine what players' changed and only update those.
-			--[[Example of output:
-					MidnightLoot.eligibleSpecs['Volstatsz-AeriePeak'] = {
-						[250] = 'Eligible',
-						[251] = 'Eligible - Offspec',
-						[252] = 'Ineligible',
-						}
-			]]--
-			local _, player, playerClass, spec1, spec2, spec3, spec4 = messageData;
-			local specs = {};
-			local eligibility = {spec1, spec2, spec3, spec4};
-			for specID, _ in ipairs(MidnightLoot.classReference[playerClass].specs) do
-				table.insert(specs, specID);
-			end
-			for i = 1, 4 do
-				if (i <= 2 or playerClass ~= "DEMONHUNTER") and (i <= 3 or playerClass == "DRUID") then
-					MidnightLoot.eligibleSpecs[player][specs[i]] = eligibility[i];
+function MidnightLoot.RollLoot(itemID, owner, kind)
+	RandomRoll(1, 100)
+	SendAddonMessage(MidnightLoot.PREFIX, "INTENT".."~"..itemID.."~"..owner.."~"..kind, "RAID")
+end
+
+
+--[[* * * * * Loot Award Decision * * * * *]]--
+
+
+function MidnightLoot.NewRoll(player, roll)
+	local intentions = MidnightLoot.intentions[player]
+	--[[We have to check if the player has a rolloff active,
+	and handle the roll differently in that case.]]
+	if MidnightLoot.activeRoster[player].isInRollOff == true then
+		table.insert(MidnightLoot.activeRoster[player].rollOffRolls, roll)
+		for index, item in ipairs(MidnightLoot.activeLootItems) do
+			if item.tiedRollers ~= nil then
+				local rollOffComplete = true
+				for _, result in ipairs(item.tiedRollers) do
+					if getn(MidnightLoot.activeRoster[result.player].rollOffRolls) == 0 then
+						rollOffComplete = false
+						break
+					end
 				end
-			end --STOPHERE (Adding chat message handling for known events.  I need to take the stuff out of "UPDATE_SPECS" and put it in its own function for consistency.)
-		elseif messageData[1] == "ERROR" then
-			table.insert(MidnightLoot.errors, {player = sender, timestamp = GetTime(), message = sendmessageData[2]});
-		end	
+				if rollOffComplete == true then
+					--This item's rolloff has rolls in from all players.
+					--[[Pop the first rolloff from each of the competing
+					players' rolloff rolls and place it in the tiedRollers
+					table, and start building a highRoller/tiedRollers again.]]
+					local highRoller = {['rollOffRoll'] = 0}
+					local tiedRollers = {}
+					for _, result in ipairs(item.tiedRollers) do
+						result.rollOffRoll = table.remove(MidnightLoot.activeRoster[result.player].rollOffRolls, 1)
+						if result.rollOffRoll > highRoller.rollOffRoll then
+							highRoller = result
+							table.wipe(tiedRollers)
+						elseif result.rollOffRoll == highRoller.rollOffRoll then
+							table.insert(tiedRollers, result)
+						end
+					end
+					--If we still have a tie, start it all over.  Otherwise, alhamdu Allah we're done and tell everyone that.
+					if getn(tiedRollers) > 0 then
+						table.insert(tiedRollers, highRoller)
+						item.tiedRollers = tiedRollers
+						for _, result in ipairs(tiedRollers) do
+							SendAddonMessage(MidnightLoot.PREFIX, ("ROLLOFF_START".."~"..item.ID.."~"..item.owner), "WHISPER", result.player)
+						end
+					else
+						item.winner = highRoller
+						MidnightLoot.RollComplete(itemID, owner)
+						item.tiedRollers = nil
+					end
+				end
+			end
+		end				
+	else
+	--[[Check if there is a matching loot intent to pair a roll to. If there
+	is, link them and start saving to results. If not, save the roll for an
+	intent to arrive.]]
+		if getn(intentions) > 0 then
+			MidnightLoot.AddResult(player, roll, table.remove(intentions, 1))
+		else
+			table.insert(MidnightLoot.rolls[player], roll)
+		end
 	end
 end
+
+function MidnightLoot.NewIntent(player, intent)
+	--Check if there is a matching loot roll to pair an intent to.
+	--If there is, link them and start saving to results.
+	--If not, save the intent for a roll to arrive.
+	local rolls = MidnightLoot.rolls[player]
+	local intent = {
+		['itemID'] = itemID,
+		['owner'] = owner,
+		['kind'] = kind
+	}
+	if getn(rolls) > 0 then
+		MidnightLoot.AddResult(player, table.remove(rolls, 1), intent)
+	else
+		table.insert(MidnightLoot.intentions[player], intent)
+	end
+end
+
+function MidnightLoot.AddResult(player, roll, intent)
+	--Take the paired roll and intent, and save them to the results table.
+	local index = MidnightLoot.GetActiveLootIndex(intent.itemID, intent.owner)
+	local result = {
+		['player'] = player,
+		['roll'] = roll,
+		['kind'] = intent.kind,
+	}
+	table.insert(MidnightLoot.activeLootItems[index].results, result)
+	--If this completes the rolls, determine the winner.
+	local results = MidnightLoot.activeLootItems[index].results
+	local totalResults = 0
+	for _, kindResults in ipairs(results) do
+		totalResults = totalResults + kindResults
+	end
+	if totalResults == getn(MidnightLoot.intentions) then
+		MidnightLoot.DetermineWinner(index)
+	end
+end
+
+function MidnightLoot.DetermineWinner(itemID, owner)
+	local index = MidnightLoot.GetActiveLootIndex(itemID, owner)
+	local item = MidnightLoot.activeLootItems[index]
+	local winner = {}
+	for _, kind in ipairs(MidnightLoot.ROLL_KINDS) do
+		if kind ~= 'pass' then
+			if getn(item.results[kind]) > 0 then
+				local highRoller = {['roll'] = 0}
+				local tiedRollers = {}
+				for _, result in ipairs(item.results[kind]) do
+					if result.roll > highRoller.roll then
+						highRoller = result
+						table.wipe(tiedRollers)
+					elseif result.roll == highRoller.roll then
+						table.insert(tiedRollers, result)
+					end
+				end
+				if getn(tiedRollers) > 0 then
+					table.insert(tiedRollers, highRoller)
+					MidnightLoot.activeLootItems[index].tiedRollers = tiedRollers
+					for _, result in ipairs(tiedRollers) do
+						SendAddonMessage(MidnightLoot.PREFIX, ("ROLLOFF_START".."~"..itemID.."~"..owner), "WHISPER", result.player)
+					end
+				else
+					winner = highRoller
+				end
+				break
+			end
+		else
+			winner = {
+				['player'] = MidnightLoot.disenchanter, 
+				['kind'] = 'Disenchant'
+			}
+		end
+	end
+	if getn(winner) > 0 then
+		MidnightLoot.activeLootItems[index].winner = winner
+		MidnightLoot.RollComplete(itemID, owner)
+	end
+end	
+
+function MidnightLoot.RollComplete(itemID, owner)
+	local index = MidnightLoot.GetActiveLootIndex(itemID, owner)
+	local item = MidnightLoot.activeLootItems[index]
+	for player, rolls in ipairs(MidnightLoot.rolls) do
+		if getn(rolls) > 0 then
+			SendAddonMessage(MidnightLoot.PREFIX, "ROLL_WARNING", "WHISPER", player)
+			table.wipe(rolls)
+		end
+	end
+	SendAddonMessage(MidnightLoot.PREFIX, "WINNER".."~"..itemID.."~"..owner.."~"..item.winner.player.."~"..item.winner.kind, "RAID")
+end
+
+--*** function MidnightLoot.ItemWon(item)
+
+--*** function MidnightLoot.TestTradeRange()
+
+--*** function MidnightLoot.TradeLoot()
